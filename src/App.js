569b5579e4985a714705ac3c16f554135464734c
hasibulc <hasibulc91@gmail.com>
2020-01-22 10:43:01 -0500

    checkpoint

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import NewPoemForm from "./NewPoemForm";
 
 const API = 'http://localhost:3000/poems'
 
+const iniNewPoem = {
+  title: "Title",
+  author: "Author",
+  content: "Write your masterpiece here..."
+}
+
 class App extends React.Component {
   
   
   state = {
     poemsArray: [],
     // readButtom: true
-    poemForm: false
+    poemForm: false,
+    newPoem: iniNewPoem
   }
 
   componentDidMount() {
@@ -29,15 +36,71 @@ class App extends React.Component {
     this.setState({poemForm: !this.state.poemForm})
   }
 
+  changeTitle = (e) => {
+    this.setState({newPoem: {
+      title: e.target.value,
+      author: this.state.newPoem.author,
+      content: this.state.newPo
+    }})
+  }
+
+  changleAuthor = (e) => {
+    this.setState({newPoem: {
+      title: this.state.newPoem.title,
+      author: e.target.value,
+      content: this.state.newPoem.content
+    }})
+  }
+
+  changeContent = (e) => {
+    this.setState({newPoem: {
+      title: this.state.newPoem.title,
+      author: this.state.newPoem.author,
+      content: e.target.value
+    }})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(this.state.newPoem)
+    // this.setState({newPoem: iniNewPoem})
+    // console.log(this.state.newPoem)
+
+    fetch(API, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state.newPoem),
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      let array = this.state.poemsArray
+      array = [...array, data]
+      this.setState({poemsArray: array})
+    })
+
+    this.setState({newPoem: iniNewPoem})
+    
+  }
+
   render() {
 
-    // console.log(this.state.poemsArray)
+    // console.log(this.state.newPoem)
 
     return (
       <div className="app">
         <div className="sidebar">
           <button onClick={this.poemFormButton}>Show/hide new poem form</button>
-          {/*false && <NewPoemForm />*/}{this.state.poemForm ? <NewPoemForm /> : null}
+          {/*false && <NewPoemForm />*/}{this.state.poemForm ? 
+            <NewPoemForm newPoem={this.state.newPoem} 
+            handleSubmit={this.handleSubmit} 
+            changeTitle={this.changeTitle}
+            changleAuthor={this.changleAuthor}
+            changeContent={this.changeContent}
+            /> 
+            : null}
         </div>
         <PoemsContainer poemsArray={this.state.poemsArray} handleClick={this.handleClick} readButtom={this.state.readButtom}/>
       </div>
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -4,10 +4,10 @@ class NewPoemForm extends React.Component {
   render() {
     return (
       <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
-        <input type="submit" value="Share your masterpiece" />
+        <input placeholder="Title" value={/*this.props.newPoem.title*/ this.props.newPoem.title} onChange={this.props.changeTitle} />
+        <input placeholder="Author" value={/*this.props.newPoem.author*/ this.props.newPoem.author} onChange={this.props.changleAuthor}/>
+        <textarea placeholder="Write your masterpiece here..." rows={10} value={/*this.props.newPoem.poem*/ this.props.newPoem.content} onChange={this.props.changeContent}/>
+        <input type="submit" value="Share your masterpiece" onClick={this.props.handleSubmit}/>
       </form>
     );
   }
